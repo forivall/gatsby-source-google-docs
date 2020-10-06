d1fe41c9c2f25473e525933d85a6b20797dd00d4
Emily Marigold Klassen <forivall@users.noreply.github.com>
2020-10-06 12:23:35 -0700

    chore: add simple usage for directly invoking write document to tests

--- utils/write-document-to-tests.js
+++ utils/write-document-to-tests.js
@@ -5,12 +5,28 @@ const _kebabCase = require("lodash/kebabCase")
 exports.writeDocumentToTests = (googleDocument) => {
   fs.writeFileSync(
     path.join(
-      process.cwd(),
+      __dirname,
       "..",
       "__tests__",
       "documents",
       `${_kebabCase(googleDocument.document.title)}.json`
     ),
-    JSON.stringify(googleDocument.document)
+    JSON.stringify(googleDocument.document, null, 2)
   )
+}
+
+if (require.main === module) {
+  ;(async function main() {
+    const GoogleOAuth2 = require("google-oauth2-env-vars")
+    const googleOAuth2 = new GoogleOAuth2({
+      token: require("./constants").ENV_TOKEN_VAR,
+    })
+    const auth = await googleOAuth2.getAuth()
+    const api = require("googleapis").google.docs({version: "v1", auth})
+    for (const id of process.argv.slice(2)) {
+      exports.writeDocumentToTests({
+        document: (await api.documents.get({documentId: id})).data,
+      })
+    }
+  })()
 }
